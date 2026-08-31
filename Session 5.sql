262efaf678d46bbe2a84971dd397df7a2080d8fe
Ferra N. <farahnadzirah92@gmail.com>
2026-08-31 11:03:09 +0800

    Add files via upload

---
+++ Session 5.sql
@@ -0,0 +1,9 @@
+# Ada berapa banyak produk yang sudah terjual? Dan berapa rata-rata retail price nya?
+
+
+
+# Tampilkan produk yang sudah terjual dengan rata-rata Retail Price dan rata-rata Sale Price
+# Diurutkan berdasarkan rata-rata retail price terbesar sampai terkecil
+
+
+
